01660cf7c263e6518bcd68a98957e1e5102652a2
Ngoc N. Hoang <nnh245@nyu.edu>
2023-03-19 23:57:20 +0400

    Uploaded data

---
+++ scripts/main.py
@@ -0,0 +1,28 @@
+# import tensorflow as tf
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+# from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+# from sklearn.model_selection import (
+#     train_test_split,
+#     cross_val_score,
+#     RepeatedStratifiedKFold,
+# )
+
+# from tensorflow import keras
+# from keras import layers, losses
+# from keras.models import Model
+
+# import random, time, sys
+
+# from ..src.models import *
+
+# class AnomalyDetection:
+
+#     def __init__(self):
+#         if len(sys.argv)
+
+import sys
+print(len(sys.argv))
--- src/models.py
+++ src/models.py
@@ -1,21 +1,7 @@
 import tensorflow as tf
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
-from sklearn.model_selection import (
-    train_test_split,
-    cross_val_score,
-    RepeatedStratifiedKFold,
-)
-
 from tensorflow import keras
-from keras import layers, losses
+from keras import layers
 from keras.models import Model
-
-import random, time
 
 
 class AutoEncoder(Model):
